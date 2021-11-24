3ce31a75a8c361bbd3aac017de75e09714b3bab5
Nicholas Cellino <nick.cellino@quadrantbiosciences.com>
2021-11-23 22:05:53 -0500

    Add repro code for nestjs swagger bug

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { HelloDto } from './app.dto';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,9 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): HelloDto {
+    return {
+      greeting: this.appService.getHello()
+    }
   }
 }
---
+++ src/app.dto.ts
@@ -0,0 +1,3 @@
+export class HelloDto {
+  greeting: string;
+}
